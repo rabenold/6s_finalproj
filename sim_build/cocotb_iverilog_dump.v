66f09489f599fb780c9b41bff1db6b281a0df5ed
Jordan Wilke <jordanwilke@DHCP-POOL-18-25-26-081.MIT.EDU>
2024-12-06 14:32:43 -0500

    Stop tracking sim_build

--- sim_build/cocotb_iverilog_dump.v
+++
@@ -1,6 +0,0 @@
-module cocotb_iverilog_dump();
-initial begin
-    $dumpfile("C:/Users/raben/Desktop/Schuul/6S/stuff/fpstuf/6s_finalproj/sim_build/rgb_conv.fst");
-    $dumpvars(0, rgb_conv);
-end
-endmodule
