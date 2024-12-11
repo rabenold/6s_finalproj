3b1eb233e23cb992c595581fb19e3ba06c110939
Rabenos <rabenold@mit.edu>
2024-12-11 02:47:54 -0500

    more robust tester

--- sim/rle_tester.py
+++ sim/rle_tester.py
@@ -21,7 +21,11 @@ async def generate_clock(clock_wire):
         await Timer(5,units="ns")
         clock_wire.value = 1
         await Timer(5,units="ns")
-
+async def reset(dut):
+    dut.rst_in.value = 1
+    await ClockCycles(dut.clk_in,1)
+    dut.rst_in.value = 0
+    await ClockCycles(dut.clk_in,1)
 
 """zig zagged in 
 [ 1,  2,  5,  9, 13, 14, 10,  6,  
@@ -72,20 +76,21 @@ def rle_encode(input_list):
 @cocotb.test() 
 async def rle_test(dut):
     print("starting test one")
-    zig_zag =[100, 95, 0, 0, 0, 80, 70, 0, 0, 0, 60, 0, 50, 0, 0, 0, 0, 40, 35, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 20, 10, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    # zig_zag =[100, 95, 0, 0, 0, 80, 70, 0, 0, 0, 60, 0, 50, 0, 0, 0, 0, 40, 35, 0, 0, 25, 0, 0, 0, 0, 0, 0, 0, 20, 10, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     # bin_zag = [bin(x)[2:] for x in zig_zag] 
     # print(bin_zag)
 
+
+    zig_zag = [random.randint(0,5) for x in range(20)]
+    zig_zag.extend([0] * (64 - len(zig_zag)))
+        
     print("=============zig zag\n\n")
     # bin_str = int_array_to_binary_string(zig_zag)
     # my_str = '0110010001011111'
     # print(run_length_encoding(bin_str))
     await cocotb.start(generate_clock(dut.clk_in))
     print("CLOCK GENERATED")
-    dut.rst_in.value = 1
-    await ClockCycles(dut.clk_in,1)
-    dut.rst_in.value = 0
-    await ClockCycles(dut.clk_in,1)
+    await reset(dut) 
     print("FEEDING IN DATA")
 #    zig_zag = [ 1,  2,  5,  9, 13, 14, 10,  6,  3,  4,  7, 11, 15, 16, 12,  8,  17, 18, 21, 25, 26, 22, 19, 20, 23, 27, 31, 32, 28, 24, 30, 29,  33, 34, 37, 41, 42, 38, 35, 36, 39, 43, 47, 48, 44, 40, 46, 45,  49, 50, 53, 57, 58, 54, 51, 52, 55, 59, 63, 64, 60, 56, 62, 61 ]
     dut.valid_in.value = 1
@@ -138,6 +143,148 @@ async def rle_test(dut):
     assert(p==f),"womp womp"
 
     print(" ===================done wit da test ====================== ")
+
+@cocotb.test()
+async def rand_test(dut):
+    await cocotb.start(generate_clock(dut.clk_in))
+    print("CLOCK GENERATED")
+
+    _1 = [random.randint(0,5) for x in range(20)]
+    _2 = [random.randint(0,5) for x in range(21)]
+    _3 = [random.randint(0,5) for x in range(22)]
+    _4 = [random.randint(0,5) for x in range(23)]
+    _5 = [random.randint(0,5) for x in range(24)]
+    _6 = [random.randint(0,5) for x in range(25)]
+    _1.extend([0] * (64 - len(_1)))
+    _2.extend([0] * (64 - len(_2)))
+    _3.extend([0] * (64 - len(_3)))
+    _4.extend([0] * (64 - len(_4)))
+    _5.extend([0] * (64 - len(_5)))
+    _6.extend([0] * (64 - len(_6)))
+        
+    print(_1)
+    print(_2)
+    dut.valid_in.value = 1
+    dut.data_in.value = _1
+    print("FED DATA IN 1")
+    await reset(dut)
+
+    while not dut.done.value:
+        await Timer(1,units="ns")
+    dut.valid_in.value = 0
+    data_out_elements_1 = dut.run_value.value
+    data_out_counts_1 = dut.run_count.value
+    _g1 = int(dut.indiv_elms.value)
+    await reset(dut)
+
+    dut.valid_in.value = 1
+    dut.data_in.value = _2
+    print("FED DATA IN 2")
+    while not dut.done.value:
+        await Timer(1,units="ns")
+    dut.valid_in.value = 0
+    data_out_elements_2 = dut.run_value.value
+    data_out_counts_2 = dut.run_count.value
+    _g2 = int(dut.indiv_elms.value)
+    await reset(dut)
+    
+    dut.valid_in.value = 1
+    dut.data_in.value = _3
+    print("FED DATA IN 3")
+    while not dut.done.value:
+        await Timer(1,units="ns")
+    dut.valid_in.value = 0
+    data_out_elements_3 = dut.run_value.value
+    data_out_counts_3 = dut.run_count.value
+    _g3 = int(dut.indiv_elms.value)
+    await reset(dut)
+
+    dut.valid_in.value = 1
+    dut.data_in.value = _4
+    print("FED DATA IN 4")
+    while not dut.done.value:
+        await Timer(1,units="ns")
+    dut.valid_in.value = 0
+    data_out_elements_4 = dut.run_value.value
+    data_out_counts_4 = dut.run_count.value
+    _g4 = int(dut.indiv_elms.value)
+    await reset(dut)
+    
+    dut.valid_in.value = 1
+    dut.data_in.value = _5
+    print("FED DATA IN 5")
+    while not dut.done.value:
+        await Timer(1,units="ns")
+    dut.valid_in.value = 0
+    data_out_elements_5 = dut.run_value.value
+    data_out_counts_5 = dut.run_count.value
+    _g5 = int(dut.indiv_elms.value)
+    await reset(dut)
+
+    dut.valid_in.value = 1
+    dut.data_in.value = _6
+    print("FED DATA IN 6")
+    while not dut.done.value:
+        await Timer(1,units="ns")
+    dut.valid_in.value = 0
+    data_out_elements_6 = dut.run_value.value
+    data_out_counts_6 = dut.run_count.value
+    _g6 = int(dut.indiv_elms.value)
+    await reset(dut)
+
+
+
+    _x1 = [int(x) for x in data_out_elements_1]
+    _y1 = [int(y) for y in data_out_counts_1]
+    _f1 = list(zip(_x1[:_g1],_y1[:_g1]))
+    _p1 = rle_encode(_1)[:_g1]
+
+
+    _x2 = [int(x) for x in data_out_elements_2]
+    _y2 = [int(y) for y in data_out_counts_2]
+    _f2 = list(zip(_x2[:_g2],_y2[:_g2]))
+    _p2 = rle_encode(_2)[:_g2]
+
+
+    _x3 = [int(x) for x in data_out_elements_3]
+    _y3 = [int(y) for y in data_out_counts_3]
+    _f3 = list(zip(_x3[:_g3],_y3[:_g3]))
+    _p3 = rle_encode(_3)[:_g3]
+
+
+    _x4 = [int(x) for x in data_out_elements_4]
+    _y4 = [int(y) for y in data_out_counts_4]
+    _f4 = list(zip(_x4[:_g4],_y4[:_g4]))
+    _p4 = rle_encode(_4)[:_g4]
+
+
+    _x5 = [int(x) for x in data_out_elements_5]
+    _y5 = [int(y) for y in data_out_counts_5]
+    _f5 = list(zip(_x5[:_g5],_y5[:_g5]))
+    _p5 = rle_encode(_5)[:_g5]
+
+
+    _x6 = [int(x) for x in data_out_elements_6]
+    _y6 = [int(y) for y in data_out_counts_6]
+    _f6 = list(zip(_x6[:_g6],_y6[:_g6]))
+    _p6 = rle_encode(_6)[:_g6]
+
+    assert(_p1==_f1),"pass 1 failed, womp womp"
+    print("==== Passed 1 ====")
+    assert(_p2==_f2),"pass 2 failed, womp womp"
+    print("==== Passed 2 ====")
+    assert(_p3==_f3),"pass 3 failed, womp womp"
+    print("==== Passed 3 ====")
+    assert(_p4==_f4),"pass 4 failed, womp womp"
+    print("==== Passed 4 ====")
+    assert(_p5==_f5),"pass 5 failed, womp womp"
+    print("==== Passed 5 ====")
+    assert(_p6==_f6),"pass 6 failed, womp womp"
+    print("==== Passed 6 ====")
+
+
+
+
 def sig_gen_runner():
     """Simulate the counter using the Python runner."""
     hdl_toplevel_lang = os.getenv("HDL_TOPLEVEL_LANG", "verilog")
